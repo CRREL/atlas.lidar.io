8cd8696dba420ef0fdf4553c87d30fca10d6dfff
Pete Gadomski <pete.gadomski@gmail.com>
2016-08-22 14:23:17 -0600

    Begin the config file specification

--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,111 @@
+extern crate iron;
 extern crate rustc_serialize;
+extern crate toml;
 
-pub enum Error {}
+use std::io::{self, Read};
+use std::fs::File;
+use std::net::{AddrParseError, SocketAddr};
+use std::path::Path;
+
+use iron::Listening;
+use iron::error::HttpResult;
+use iron::prelude::*;
+use rustc_serialize::Decodable;
+use toml::{DecodeError, Decoder, Parser, ParserError, Value};
+
+#[derive(Debug)]
+pub enum Error {
+    TomlDecode(DecodeError),
+    ParseConfig(Vec<ParserError>),
+    Io(io::Error),
+    NetAddrParse(AddrParseError),
+}
+
+impl From<io::Error> for Error {
+    fn from(err: io::Error) -> Error {
+        Error::Io(err)
+    }
+}
+
+impl From<AddrParseError> for Error {
+    fn from(err: AddrParseError) -> Error {
+        Error::NetAddrParse(err)
+    }
+}
+
+impl From<DecodeError> for Error {
+    fn from(err: DecodeError) -> Error {
+        Error::TomlDecode(err)
+    }
+}
 
 pub type Result<T> = std::result::Result<T, Error>;
 
 #[derive(Debug, RustcDecodable)]
-pub struct Config;
+struct Config {
+    server: ServerConfig,
+}
 
-pub struct Server;
+impl Config {
+    fn from_path<P: AsRef<Path>>(path: P) -> Result<Config> {
+        let mut string = String::new();
+        {
+            let mut file = try!(File::open(path));
+            try!(file.read_to_string(&mut string));
+        }
+        let mut parser = Parser::new(&string);
+        let toml = match parser.parse() {
+            Some(toml) => Value::Table(toml),
+            None => return Err(Error::ParseConfig(parser.errors.clone())),
+        };
+        Config::decode(&mut Decoder::new(toml)).map_err(Error::from)
+    }
+}
+
+#[derive(Debug, RustcDecodable)]
+struct ServerConfig {
+    ip: String,
+    port: u16,
+}
+
+#[derive(Debug)]
+pub struct Server {
+    addr: SocketAddr,
+}
 
 impl Server {
-    pub fn new(config: Config) -> Server {
+    /// Reads a server configuration file and creates the server.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// # use atlas_lidar_io::Server;
+    /// let server = Server::from_path("data/server.toml").unwrap();
+    /// ```
+    pub fn from_path<P: AsRef<Path>>(path: P) -> Result<Server> {
+        let config = try!(Config::from_path(path));
+        let ip = try!(config.server.ip.parse());
+        Ok(Server { addr: SocketAddr::new(ip, config.server.port) })
+    }
+
+    /// Starts the server.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// # use atlas_lidar_io::Server;
+    /// let server = Server::from_path("data/server.toml").unwrap();
+    /// server.serve().unwrap();
+    /// ```
+    pub fn serve(&self) -> HttpResult<Listening> {
+        Iron::new(self.chain()).http(self.addr())
+    }
+
+    fn chain(&self) -> Chain {
         unimplemented!()
     }
 
-    pub fn serve(&self) -> Result<()> {
-        unimplemented!()
+    fn addr(&self) -> SocketAddr {
+        self.addr
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -3,15 +3,11 @@ extern crate docopt;
 extern crate rustc_serialize;
 extern crate toml;
 
-use std::fs::File;
-use std::io::Read;
 use std::path::PathBuf;
 
 use docopt::Docopt;
-use rustc_serialize::Decodable;
-use toml::{Decoder, Parser, Value};
 
-use atlas_lidar_io::{Config, Server};
+use atlas_lidar_io::Server;
 
 const USAGE: &'static str = "
 ATLAS HTTP server.
@@ -34,25 +30,6 @@ fn main() {
         .map(|d| d.version(option_env!("CARGO_PKG_VERSION").map(|s| s.to_string())))
         .and_then(|d| d.decode())
         .unwrap();
-    let mut toml = String::new();
-    {
-        let mut file = File::open(&args.arg_config)
-            .expect(&format!("Unable to open configuration file: {}",
-                             args.arg_config.to_string_lossy()));
-        file.read_to_string(&mut toml).unwrap();
-    }
-    let mut parser = Parser::new(&toml);
-    let toml = match parser.parse() {
-        Some(table) => Value::Table(table),
-        None => {
-            for error in &parser.errors {
-                println!("Parser error: {}", error);
-            }
-            std::process::exit(1);
-        }
-    };
-    let mut decoder = Decoder::new(toml);
-    let config = Config::decode(&mut decoder).unwrap();
-    let server = Server::new(config);
-    server.serve();
+    let server = Server::from_path(&args.arg_config).unwrap();
+    server.serve().unwrap();
 }
