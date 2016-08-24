94cc5347083084ffcfb66f61a7ba49c1f98e2ee9
Pete Gadomski <pete.gadomski@gmail.com>
2016-08-24 16:20:07 -0600

    Graphs

--- src/api.rs
+++ src/api.rs
@@ -1,3 +1,6 @@
+use std::fmt::Write;
+
+use heartbeat::Heartbeat;
 use heartbeat::units::Percentage;
 use iron::Handler;
 use iron::headers::ContentType;
@@ -15,7 +18,8 @@ pub struct Api {
 impl Api {
     pub fn new(world: World) -> Api {
         let mut router = Router::new();
-        router.get("/soc", Soc { world: world });
+        router.get("/soc", CsvHandler::new(world.clone(), Soc));
+        router.get("/temperature", CsvHandler::new(world, Temperature));
         Api { router: router }
     }
 }
@@ -26,19 +30,31 @@ impl Handler for Api {
     }
 }
 
-struct Soc {
+trait Csv {
+    fn header(&self) -> &str;
+    fn row(&self, heartbeat: &Heartbeat) -> String;
+}
+
+struct CsvHandler<C: Csv> {
+    csv: C,
     world: World,
 }
 
-impl Handler for Soc {
+impl<C: Csv> CsvHandler<C> {
+    fn new(world: World, csv: C) -> CsvHandler<C> {
+        CsvHandler {
+            world: world,
+            csv: csv,
+        }
+    }
+}
+
+impl<C: 'static + Csv + Send + Sync> Handler for CsvHandler<C> {
     fn handle(&self, _: &mut Request) -> IronResult<Response> {
         let mut data = String::new();
-        data.push_str("Datetime,Battery #1,Battery #2\n");
+        itry!(writeln!(data, "{}", self.csv.header()));
         for heartbeat in self.world.heartbeats.read().unwrap().iter() {
-            data.push_str(&format!("{},{:.2},{:.2}\n",
-                                   heartbeat.start_time.to_string(),
-                                   Percentage::from(heartbeat.soc1).0,
-                                   Percentage::from(heartbeat.soc2).0));
+            itry!(writeln!(data, "{}", self.csv.row(heartbeat)));
         }
         let mut response = Response::new();
         response.set_mut(status::Ok)
@@ -46,5 +62,37 @@ impl Handler for Soc {
         response.headers
             .set(ContentType(Mime(TopLevel::Text, SubLevel::Ext("csv".to_string()), vec![])));
         Ok(response)
+    }
+}
+
+struct Soc;
+
+impl Csv for Soc {
+    fn header(&self) -> &str {
+        "Datetime,Battery #1,Battery #2"
+    }
+    fn row(&self, heartbeat: &Heartbeat) -> String {
+        format!("{},{:.2},{:.2}",
+                heartbeat.start_time,
+                Percentage::from(heartbeat.soc1).0,
+                Percentage::from(heartbeat.soc2).0)
+    }
+}
+
+struct Temperature;
+
+impl Csv for Temperature {
+    fn header(&self) -> &str {
+        "Datetime,External,Mount,Scanner"
+    }
+    fn row(&self, heartbeat: &Heartbeat) -> String {
+        let mut row = format!("{},{},{},",
+                              heartbeat.start_time,
+                              heartbeat.external_temperature.0,
+                              heartbeat.mount_temperature.0);
+        if let Some(scan_on) = heartbeat.last_scan_on {
+            row.push_str(&format!("{:2}", scan_on.scanner_temperature.0));
+        }
+        row
     }
 }
--- static/index.js
+++ static/index.js
@@ -20,3 +20,21 @@ var soc_graph = new Dygraph(document.getElementById("soc-graph"), "/api/v1/soc",
     }
   }
 });
+
+var temperature_graph = new Dygraph(document.getElementById("temperature-graph"), "/api/v1/temperature", {
+  dateWindow: [Date.now() - 2 * 30 * 24 * 60 * 60 * 1000, Date.now()],
+  height: 300,
+  labelsUTC: true,
+  rollPeriod: 24,
+  showRangeSelector: true,
+  showRoller: true,
+  axes: {
+    y: {
+      axisLabelFormatter: function(y) {
+        return y + "&deg;C";
+      }
+    }
+  }
+});
+
+var _ = Dygraph.synchronize(soc_graph, temperature_graph, { range: false });
