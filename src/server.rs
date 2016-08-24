9e690b44e63b4259fcc4ee2e38b4c88cc5d8a55a
Pete Gadomski <pete.gadomski@gmail.com>
2016-08-24 08:13:43 -0600

    Remove status, for now
    We're rebuilding, yo!

--- src/server.rs
+++ src/server.rs
@@ -64,7 +64,6 @@ impl Server {
             registry.register_helper("millibars", Box::new(helper::millibars));
             registry.register_helper("percentage", Box::new(helper::percentage));
             registry.register_helper("orion-percentage", Box::new(helper::orion_percentage));
-            registry.register_helper("li-status", Box::new(helper::li_status));
         }
         chain.link_after(maybe_watch_handlebars_engine(&config.template_directory,
                                                        handlebars_engine));
