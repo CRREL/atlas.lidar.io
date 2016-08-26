926b48abfd1a7db5c1f80c722fccf9c0764964de
Pete Gadomski <pete.gadomski@gmail.com>
2016-08-26 06:00:37 -0600

    Add supervisord reload command

--- fabfile.py
+++ fabfile.py
@@ -2,13 +2,16 @@ from fabric.api import cd, env, local, sudo, task, run
 
 env.hosts = ["lidar.io"]
 env.use_ssh_config = True
-REMOTE_DIRECTORY = "/var/www/atlas.lidar.io"
 
 @task
 def deploy():
     local("cargo build")
     local("cargo test")
-    with cd(REMOTE_DIRECTORY):
+    with cd("/var/www/atlas.lidar.io"):
         run("git pull")
         run("cargo build --release")
         sudo("supervisorctl restart atlas.lidar.io")
+
+@task
+def reload():
+    sudo("supervisorctl reload")
