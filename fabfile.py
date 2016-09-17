09fa6b8de4cf5ab37d1a73f30e5b069aa2d68515
Pete Gadomski <pete.gadomski@gmail.com>
2016-09-17 13:07:26 -0600

    Allow deploy to non-master branches fab

--- fabfile.py
+++ fabfile.py
@@ -4,11 +4,13 @@ env.hosts = ["lidar.io"]
 env.use_ssh_config = True
 
 @task
-def deploy():
+def deploy(branch="master"):
+    local("git checkout {}".format(branch))
     local("cargo build")
     local("cargo test")
     with cd("/var/www/atlas.lidar.io"):
-        run("git checkout master")
+        run("git fetch origin")
+        run("git checkout {}".format(branch))
         run("git pull")
         run("cargo build --release")
         sudo("supervisorctl restart atlas.lidar.io")
