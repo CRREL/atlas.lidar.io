2a0b9be5e0aaaa8a5e5ee286159539a69917551b
Pete Gadomski <pete.gadomski@gmail.com>
2017-04-10 13:47:24 -0600

    Set environment variables for building on deploy

--- fabfile.py
+++ fabfile.py
@@ -1,4 +1,4 @@
-from fabric.api import cd, env, local, sudo, task, run
+from fabric.api import cd, env, local, sudo, task, run, shell_env, path
 
 env.hosts = ["lidar.io"]
 env.use_ssh_config = True
@@ -9,11 +9,14 @@ def deploy(branch="master"):
     local("cargo build")
     local("cargo test")
     with cd("/var/www/atlas.lidar.io"):
-        run("git fetch origin")
-        run("git checkout {}".format(branch))
-        run("git pull")
-        run("cargo build --release")
-        sudo("supervisorctl restart atlas.lidar.io")
+        with shell_env(PKG_CONFIG_PATH="/usr/local/lib/pkgconfig",
+                       LD_LIBRARY_PATH="/home/pgadomski/local/lib64",):
+            with path("/home/pgadomski/local/bin", behavior="prepend"):
+                run("git fetch origin")
+                run("git checkout {}".format(branch))
+                run("git pull")
+                run("cargo build --release")
+                sudo("supervisorctl restart atlas.lidar.io")
 
 @task
 def reload():
