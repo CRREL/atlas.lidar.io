55f941fa4dae8278ba37e5330350c3dd458bab93
Pete Gadomski <pete.gadomski@gmail.com>
2017-08-04 13:19:21 -0600

    Okay, bootstrap was a good call

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+
+import { Camera } from "./camera";
+import { CameraService } from "./camera.service";
 
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.css"],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = "ATLAS";
+  cameras: Camera[] = [];
+
+  constructor(private cameraService: CameraService) { }
+
+  ngOnInit(): void {
+    this.cameraService.getCameras()
+      .then(cameras => this.cameras = cameras as Camera[]);
+  }
 }
