ef9489f75ecf888c0a29c2ad118e9eb15309ece7
Pete Gadomski <pete.gadomski@gmail.com>
2017-09-05 13:47:52 -0600

    Update deploy link

--- fabfile.py
+++ fabfile.py
@@ -1,6 +1,6 @@
 from fabric.api import local, put, env, task
 
-REMOTE_DIRECTORY = "/var/www/atlas.lidar.io-dev"
+REMOTE_DIRECTORY = "/var/www/atlas.lidar.io"
 
 env.hosts = ["lidar.io"]
 env.use_ssh_config = True
