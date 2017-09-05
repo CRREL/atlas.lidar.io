e5c027ea83bf6d3747f8de76968ea2e255c1f07d
Pete Gadomski <pete.gadomski@gmail.com>
2017-09-05 11:19:58 -0600

    Add efoy to background of chart

--- src/app/atlas-power-history.ts
+++ src/app/atlas-power-history.ts
@@ -2,4 +2,6 @@ export class AtlasPowerHistory {
   datetime: Date[];
   state_of_charge_1: number[];
   state_of_charge_2: number[];
+  efoy_1_current: number[];
+  efoy_2_current: number[];
 }
--- src/app/atlas-power.component.ts
+++ src/app/atlas-power.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, ViewChild, ViewEncapsulation, ElementRef } from "@an
 import { AtlasService } from "./atlas.service";
 import { AtlasPowerHistory } from "./atlas-power-history";
 import * as d3 from "d3";
+import * as d3_scale_chromatic from "d3-scale-chromatic";
 
 @Component({
   selector: "atlas-power",
@@ -26,13 +27,21 @@ export class AtlasPowerComponent implements OnInit {
   createChart(): void {
     const batteries = [{
       name: "Battery 1",
-      values: this.datify(this.history.state_of_charge_1),
+      values: this.datifyPowerRecord(this.history.state_of_charge_1),
     },
     {
       name: "Battery 2",
-      values: this.datify(this.history.state_of_charge_2),
+      values: this.datifyPowerRecord(this.history.state_of_charge_2),
     }
     ];
+    const efoys = [{
+      name: "EFOY 1",
+      values: this.datifyEfoyRecord(this.history.efoy_1_current),
+    },
+    {
+      name: "EFOY 2",
+      values: this.datifyEfoyRecord(this.history.efoy_2_current),
+    }];
     const element = this.chartContainer.nativeElement;
     const width = element.offsetWidth - this.margin.left - this.margin.right;
     const height = element.offsetHeight - this.margin.top - this.margin.bottom;
@@ -43,21 +52,30 @@ export class AtlasPowerComponent implements OnInit {
 
     const g = svg.append("g").attr("transform", `translate(${this.margin.left},${this.margin.top})`);
     const xScale = d3.scaleTime().domain(d3.extent(this.history.datetime)).range([0, width]);
-    const yScale = d3.scaleLinear().domain([0, 100]).rangeRound([height, 0]);
-    const zScale = d3.scaleOrdinal(d3.schemeCategory10).domain(batteries.map(b => b.name));
+    const yScaleBattery = d3.scaleLinear().domain([0, 100]).rangeRound([height, 0]);
+    const yScaleEfoy = d3.scaleLinear().domain([0, 4]).rangeRound([height, 0]);
+    const zScaleBatteries = d3.scaleOrdinal(d3_scale_chromatic.schemePaired).domain(batteries.map(b => b.name));
+    const zScaleEfoys = d3.scaleOrdinal(d3_scale_chromatic.schemePastel1).domain(efoys.map(b => b.name));
     const line = d3.line<PowerRecord>()
       .curve(d3.curveBasis)
       .x(d => xScale(d.datetime))
-      .y(d => yScale(d.state_of_charge));
+      .y(d => yScaleBattery(d.state_of_charge));
+    const area = d3.area<EfoyRecord>()
+      .curve(d3.curveBasis)
+      .x(d => xScale(d.datetime))
+      .y0(yScaleEfoy(0))
+      .y1(d => yScaleEfoy(d.current));
 
-    g.append("g")
-      .attr("class", "axis axis-x")
-      .attr("transform", `translate(0,${height})`)
-      .call(d3.axisBottom(xScale));
+    var efoy = g.selectAll(".efoy")
+      .data(efoys)
+      .enter().append("g")
+      .attr("class", "efoy");
 
-    g.append("g")
-      .attr("class", "axis axis-y")
-      .call(d3.axisLeft(yScale));
+    efoy.append("path")
+      .attr("class", "area")
+      .attr("d", d => area(d.values))
+      .style("opacity", 0.6)
+      .style("fill", d => zScaleEfoys(d.name));
 
     var battery = g.selectAll(".battery")
       .data(batteries)
@@ -67,13 +85,29 @@ export class AtlasPowerComponent implements OnInit {
     battery.append("path")
       .attr("class", "line")
       .attr("d", d => line(d.values))
-      .style("stroke", d => zScale(d.name));
+      .style("stroke", d => zScaleBatteries(d.name));
+
+    g.append("g")
+      .attr("class", "axis axis-x")
+      .attr("transform", `translate(0,${height})`)
+      .call(d3.axisBottom(xScale));
+
+    g.append("g")
+      .attr("class", "axis axis-y")
+      .call(d3.axisLeft(yScaleBattery));
   }
 
-  private datify(state_of_charge: number[]): PowerRecord[] {
+  private datifyPowerRecord(state_of_charge: number[]): PowerRecord[] {
     return this.history.datetime.map((datetime, i) => {
       var powerRecord = { datetime: datetime, state_of_charge: state_of_charge[i] };
       return powerRecord;
+    });
+  }
+
+  private datifyEfoyRecord(current: number[]): EfoyRecord[] {
+    return this.history.datetime.map((datetime, i) => {
+      var efoyRecord = { datetime: datetime, current: current[i] };
+      return efoyRecord;
     });
   }
 }
@@ -81,4 +115,9 @@ export class AtlasPowerComponent implements OnInit {
 interface PowerRecord {
   datetime: Date;
   state_of_charge: number;
+}
+
+interface EfoyRecord {
+  datetime: Date;
+  current: number;
 }
