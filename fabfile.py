60a48e9ee1b3c5346007dfa2cecb7c04bc9d893c
Pete Gadomski <pete.gadomski@gmail.com>
2017-09-05 12:06:51 -0600

    Add fabfile

---
+++ fabfile.py
@@ -0,0 +1,22 @@
+from fabric.api import local, put, env, task
+
+REMOTE_DIRECTORY = "/var/www/atlas.lidar.io-dev"
+
+env.hosts = ["lidar.io"]
+env.use_ssh_config = True
+
+
+@task
+def deploy():
+    build()
+    upload()
+
+
+@task
+def build():
+    local("ng build")
+
+
+@task
+def upload():
+    put("dist/*", REMOTE_DIRECTORY)
