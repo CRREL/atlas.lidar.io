ecebe5c03f6e0fc851b44ec350eb820df34e1cde
Pete Gadomski <pete.gadomski@gmail.com>
2017-09-05 09:30:31 -0600

    Add efoy information to site

---
+++ src/app/atlas-efoy.ts
@@ -0,0 +1,8 @@
+export interface AtlasEfoy {
+  id: number;
+  state: string;
+  cartridge: string;
+  consumed: number;
+  voltage: number;
+  current: number;
+}
--- src/app/atlas-status.ts
+++ src/app/atlas-status.ts
@@ -1,6 +1,8 @@
 import { AtlasBattery } from "./atlas-battery";
+import { AtlasEfoy } from "./atlas-efoy";
 
 export interface AtlasStatus {
   last_heartbeat_received: Date;
   batteries: AtlasBattery[];
+  efoys: AtlasEfoy[];
 }
