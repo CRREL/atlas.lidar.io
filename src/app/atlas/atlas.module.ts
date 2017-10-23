077dccb25819f06e782a9549b85a6e0022fb0451
Pete Gadomski <pete.gadomski@gmail.com>
2017-10-23 16:08:58 -0600

    Reorganize the atlas.module

--- src/app/atlas/atlas.module.ts
+++ src/app/atlas/atlas.module.ts
@@ -4,28 +4,30 @@ import { CommonModule } from '@angular/common';
 import { SharedModule } from '../shared/shared.module';
 import { CameraModule } from '../camera/camera.module';
 
-import { AtlasService } from './atlas.service';
-import { AtlasRoutingModule } from './atlas-routing.module';
-
-import { AtlasDashboardComponent } from './atlas-dashboard.component';
 import { AtlasBatteriesComponent } from './atlas-batteries.component';
-import { AtlasEfoysComponent } from './atlas-efoys.component';
-import { AtlasTimeseriesComponent } from './atlas-timeseries.component';
-import { AtlasStatusComponent } from './atlas-status.component';
 import { AtlasCamerasComponent } from './atlas-cameras.component';
+import { AtlasDashboardComponent } from './atlas-dashboard.component';
+import { AtlasEfoysComponent } from './atlas-efoys.component';
+import { AtlasRoutingModule } from './atlas-routing.module';
+import { AtlasService } from './atlas.service';
+import { AtlasStatusComponent } from './atlas-status.component';
+import { AtlasTimeseriesComponent } from './atlas-timeseries.component';
 
 @NgModule({
   imports: [
-    CommonModule, AtlasRoutingModule, SharedModule, CameraModule
+    AtlasRoutingModule,
+    CameraModule,
+    CommonModule,
+    SharedModule,
   ],
   exports: [],
   declarations: [
-    AtlasDashboardComponent,
     AtlasBatteriesComponent,
-    AtlasEfoysComponent,
-    AtlasTimeseriesComponent,
-    AtlasStatusComponent,
     AtlasCamerasComponent,
+    AtlasDashboardComponent,
+    AtlasEfoysComponent,
+    AtlasStatusComponent,
+    AtlasTimeseriesComponent,
   ],
   providers: [AtlasService],
 })
