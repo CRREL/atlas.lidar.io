e784c6525fd850fbd660c164878e9129df579d22
Pete Gadomski <pete.gadomski@gmail.com>
2018-08-02 15:49:51 -0400

    Remove atlas north b/c its not in the api

--- src/app/atlas/atlas-dashboard.component.ts
+++ src/app/atlas/atlas-dashboard.component.ts
@@ -13,7 +13,7 @@ import { CameraService } from '../camera/camera.service';
 export class AtlasDashboardComponent implements OnInit {
     atlasStatus: AtlasStatus;
     cameras: Camera[];
-    cameraNames = ['ATLAS_CAM', 'ATLAS_CAM2', 'HEL_ATLASNORTH', 'HEL_DUAL_2', 'HEL_DUAL_1', 'HEL_BERGCAM3_1', 'HEL_BERGCAM3_2'];
+    cameraNames = ['ATLAS_CAM', 'ATLAS_CAM2', 'HEL_DUAL_2', 'HEL_DUAL_1', 'HEL_BERGCAM3_1', 'HEL_BERGCAM3_2'];
 
     constructor(private atlasService: AtlasService, private cameraService: CameraService) { }
 
