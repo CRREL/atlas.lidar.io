7f651b28f10e52899575fffa19c92f3f920bd711
Pete Gadomski <pete.gadomski@gmail.com>
2018-08-02 08:07:28 -0400

    Add HEL_ATLASNORTH to cameras

--- src/app/atlas/atlas-dashboard.component.ts
+++ src/app/atlas/atlas-dashboard.component.ts
@@ -6,21 +6,21 @@ import { Camera } from '../camera/camera';
 import { CameraService } from '../camera/camera.service';
 
 @Component({
-  selector: 'app-atlas-dashboard',
-  templateUrl: './atlas-dashboard.component.html',
-  styleUrls: ['./atlas-dashboard.component.css'],
+    selector: 'app-atlas-dashboard',
+    templateUrl: './atlas-dashboard.component.html',
+    styleUrls: ['./atlas-dashboard.component.css'],
 })
 export class AtlasDashboardComponent implements OnInit {
-  atlasStatus: AtlasStatus;
-  cameras: Camera[];
-  cameraNames = ['ATLAS_CAM', 'ATLAS_CAM2', 'HEL_DUAL_2', 'HEL_DUAL_1', 'HEL_BERGCAM3_1', 'HEL_BERGCAM3_2'];
+    atlasStatus: AtlasStatus;
+    cameras: Camera[];
+    cameraNames = ['ATLAS_CAM', 'ATLAS_CAM2', 'HEL_ATLASNORTH', 'HEL_DUAL_2', 'HEL_DUAL_1', 'HEL_BERGCAM3_1', 'HEL_BERGCAM3_2'];
 
-  constructor(private atlasService: AtlasService, private cameraService: CameraService) { }
+    constructor(private atlasService: AtlasService, private cameraService: CameraService) { }
 
-  ngOnInit(): void {
-    this.atlasService.getAtlasStatus()
-      .then(atlasStatus => this.atlasStatus = atlasStatus);
-    this.cameraService.getCameraDetails(this.cameraNames)
-      .then(cameras => this.cameras = cameras);
-  }
+    ngOnInit(): void {
+        this.atlasService.getAtlasStatus()
+            .then(atlasStatus => this.atlasStatus = atlasStatus);
+        this.cameraService.getCameraDetails(this.cameraNames)
+            .then(cameras => this.cameras = cameras);
+    }
 }
