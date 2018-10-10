59e40c5bf1c0ab46147c3a27fd0bd653d7d6635a
Pete Gadomski <pete.gadomski@gmail.com>
2018-10-10 14:15:53 -0600

    Add an image picker

--- src/components/Camera.vue
+++ src/components/Camera.vue
@@ -6,16 +6,25 @@
     {{ camera.description }}
     </p>
 
-    <div v-if="images">
-      {{ images[0] }}
-    </div>
+    <b-row>
+      <b-col offset="3" cols="6">
+        <b-img :src="camera.latest_image.url" fluid />
+      </b-col>
+    </b-row>
+
+    <image-picker v-if="images" :tree="images">
+    </image-picker>
   </div>
 </template>
 
 <script>
 import api from '../api'
+import ImagePicker from './ImagePicker.vue'
 
 export default {
+  components: {
+    ImagePicker
+  },
   data () {
     return {
       images: null
@@ -29,14 +38,35 @@ export default {
   },
   methods: {
     fetchImages: function () {
-      api.cameraImages(this.id, 0).then(response => { this.images = response.data })
+      api.cameraImages(this.id, 0).then(response => { this.images = this.buildTree(response.data) })
+    },
+    buildTree: function (images) {
+      let tree = {}
+      for (let i = 0; i < images.length; i++) {
+        const image = images[i]
+        const date = new Date(image.datetime)
+        const year = date.getFullYear()
+        const month = date.getMonth()
+        const day = date.getDay()
+        if (!tree[year]) {
+          tree[year] = {}
+        }
+        if (!tree[year][month]) {
+          tree[year][month] = {}
+        }
+        if (!tree[year][month][day]) {
+          tree[year][month][day] = []
+        }
+        tree[year][month][day].push(image)
+      }
+      return tree
     }
   },
   mounted () {
     this.fetchImages()
   },
   watch: {
-    id: function (_newId, _oldId) {
+    id: function () {
       this.images = null
       this.fetchImages()
     }
---
+++ src/components/ImagePicker.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="image-picker">
+    {{ years }}
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['tree'],
+  computed: {
+    years () {
+      let years = Object.keys(this.tree)
+      years.sort()
+      return years
+    }
+  }
+}
+</script>
